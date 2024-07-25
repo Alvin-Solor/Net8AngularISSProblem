2a72339d388607074b0dbb2f48da114d4e8dbd76
Alvin-Solor <AlvinS411@gmail.com>
2024-07-25 13:34:40 -0400

    work

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Authentication.Negotiate;
+
+namespace Net8AngularWithIISWindowsDomainUserIdentity.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme).AddNegotiate();
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.MapFallbackToFile("/index.html");
+
+            app.Run();
+        }
+    }
+}
